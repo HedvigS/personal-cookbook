f79d96e9f5056fe5a53f02437656036e0eb2a5e1
Hedvig Skirgård <hedvig.skirgard@gmail.com>
2021-03-27 18:47:44 +0100

    Create stripping_names_to_ascii.R

---
+++ R/stripping_names_to_ascii.R
@@ -0,0 +1,32 @@
+#This scripts removes non-ascii characters from language names so that they are more compatable with certain programs, like SplitsTree
+
+#installing and loading packages
+if (!suppressPackageStartupMessages(require("pacman"))) { install.packages("pacman") } #if pacman isn't already installed, install it.
+
+pacman::p_load(
+  dplyr,#for data wrangling
+  stringr, #for manipulating strings
+  stringi, #for latin-ascii conversion
+  readr #for reading in data files
+)
+
+options(tidyverse.quiet = TRUE)
+
+#combining the tables languages and values from glottolog-cldf into one wide dataframe
+source("make_lang_values_wide_fetch_online.R.R")
+glottolog <- read_tsv("cldf_wide_df.tsv")
+
+#making columns with the names of languages, but stripped so it won't cause trouble in applications like SplitsTree
+glottolog$Name_stripped <- glottolog$Name %>% 
+  stringi::stri_trans_general("latin-ascii") %>% 
+  str_replace_all("\\(", "") %>%  
+  str_replace_all("\\)", "") %>% 
+  str_replace_all("\\-", "") %>% 
+  str_replace_all("\\'", "?")
+
+glottolog$Name_stripped_no_spaces <- glottolog$Name_stripped %>% 
+  str_replace_all(" ", "_")  
+
+glottolog %>% 
+  dplyr::select(Language_ID, Name, Name_stripped, Name_stripped_no_spaces) %>% 
+  write_tsv("glottolog_names_stripped.tsv")
