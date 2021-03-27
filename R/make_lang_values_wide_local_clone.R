09fc35618d6a88e6c89deaf154ceb68cfdec8087
Hedvig Skirgård <hedvig.skirgard@gmail.com>
2021-03-27 18:38:10 +0100

    split widening files into two (internet vs clone)

---
+++ R/make_lang_values_wide_local_clone.R
@@ -0,0 +1,39 @@
+#This script takes the values and languages tables from a cldf-release and combines then and transforms them to a wide data format from a long. It does not take into account the parameter or code tables.
+
+#you need to set cldf_folder folder to wherever the clone of the cldf dataset you want to widen is
+
+if (!suppressPackageStartupMessages(require("pacman"))) { install.packages("pacman") } #if pacman isn't already installed, install it.
+
+pacman::p_load(
+  dplyr,#for data wrangling
+  jsonlite, #reading json files
+  stringr, #for string evaluation
+  readr #for reading in data files
+)
+
+pacman::p_load_gh("SimonGreenhill/rcldf")
+
+#reading in cldf data via rcldf
+cldf_folder <- "../../glottolog-cldf/cldf/"
+
+cldf_json <- paste0(cldf_folder, "cldf-metadata.json")
+
+cldf_object <- rcldf::cldf(cldf_json)
+
+values <- cldf_object$tables$values %>% 
+  reshape2::dcast(Language_ID ~ Parameter_ID, value.var = "Value") #making long data wide
+
+languages <- cldf_object$tables$languages
+
+#The languages-table from glottolog-cldf contains a paramter called "Language_ID" which is NOT the same as the parameter "Language_ID" in the values tables. This parameter is in fact the language leveled parent of a dialect. In order to avoid confusion, let's rename the parameter in the languages tables to the more transparent "Language_level_ID". This set-up first test if this is indeed a problem (i.e. if this is glottolog-cldf) and only does the renaming then.
+
+if(str_detect(cldf_folder, "glottolog")) {
+  languages <- languages %>% 
+    rename(Language_level_ID = Language_ID) %>% 
+    rename(Language_ID = ID)
+} else{  languages <- languages %>% 
+  rename(Language_ID = ID)}
+
+cldf_wide_df <- dplyr::full_join(values,languages) 
+
+write_tsv(cldf_wide_df, "cldf_wide_df.tsv")
