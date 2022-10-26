bbdbac6a2b94001a7dde6a9691d02788f24fe155
Hedvig Skirgård <hedvig.skirgard@gmail.com>
2022-10-26 18:19:49 +0200

    Create cognate_lv_dists_assignment_sus.R

---
+++ R/cognate_lv_dists_assignment_sus.R
@@ -0,0 +1,90 @@
+library(tidyverse)
+library(cluster)
+library(reshape2)
+library(stringdist)
+
+forms <- read_csv("https://raw.githubusercontent.com/lexibank/abvd/master/cldf/forms.csv")
+
+unknown_cognacy <-  forms %>% 
+  filter(is.na(Cognacy)) %>% 
+  dplyr::select(ID_Var1= ID, Var1 = Form)  %>% 
+  distinct()
+
+known_cognacy <- forms %>% 
+  filter(!is.na(Cognacy)) %>% 
+  dplyr::select(Var2 = Form, Var2_Cognacy = Cognacy)  %>% 
+  distinct()
+  
+#make df to join to in loop
+dist_full <- matrix(nrow = 0, ncol = 3) %>% 
+  as.data.frame() %>% 
+  rename("Var1" = V1, "Var2" = V2, "lv_dist"= V3) %>% 
+  mutate(Var1 = as.character(Var1)) %>% 
+  mutate(Var2 = as.character(Var2)) %>% 
+  mutate(lv_dist = as.numeric(lv_dist))
+
+##
+
+
+left <- forms %>%
+dplyr::select(Var1 = Form, ID_1 = ID, Cognacy_1 = Cognacy)
+right <- forms %>%
+dplyr::select(Var2 = Form, ID_2 = ID, Cognacy_2 = Cognacy)
+
+Parameters_ID_unique_vector <- forms$Parameter_ID %>% unique()
+
+index <- 0
+
+for(Paramter in Parameters_ID_unique_vector){
+
+index <- index + 1
+
+cat(paste0("I'm on ", Parameters_ID_unique_vector[index], ". Which is index ", index, " out of ", length(Parameters_ID_unique_vector), ".\n"))
+
+forms_spec <- forms %>%
+filter(Parameter_ID == Parameters_ID_unique_vector[index])
+
+form_vec <- as.vector(forms_spec$Form)
+
+names(form_vec) <- forms_spec$ID
+
+dists <- stringdistmatrix(a = form_vec, b = form_vec, method = "lv",  useNames = "names")
+
+dists[upper.tri(dists, diag = T)] <- NA
+
+dists_long <- dists %>%
+reshape2::melt() %>%
+filter(!is.na(value)) %>%
+filter(value <= 1)  %>%
+distinct() %>%
+mutate(Var1 = as.character(Var1)) %>%
+mutate(Var2 = as.character(Var2)) %>%
+rename(lv_dist = value) %>%
+left_join(left, by = "Var1") %>%
+left_join(right, by = "Var2") %>%
+distinct()
+
+dist_full <- full_join(dist_full, dists_long, by = c("Var1", "Var2", "lv_dist"))
+
+}
+
+
+joined %>%
+  filter(lv_dist ==0) %>% 
+  distinct(Var1) %>% nrow()
+
+
+#different cognate same form
+different_cognate_same_form <- forms %>% 
+  distinct(Form_1, Parameter_ID, Cognacy) %>% 
+  group_by(Form_1, Parameter_ID) %>%
+  summarise(n = n()) %>% 
+  filter(n > 1) %>% 
+  distinct(Form_1, Parameter_ID)
+
+
+#compare all dists
+
+dists_sided <- dist_full %>% 
+  left_join(left) %>% 
+  left_join(right)
