14e9ed3ea30226107f52e1d28fd170b43168bb61
Hedvig Skirgård <hedvig.skirgard@gmail.com>
2023-02-03 15:58:44 +0100

    adde comments

--- R/check_used_functions.R
+++ R/check_used_functions.R
@@ -1,46 +1,55 @@
+#loading necessary packages
 source("fun_def_h_load.R")
-
 h_load("NCmisc")
 h_load("tidyverse")
 
+#making a list of all r-scripts in this directory, including down in sub-directories
 r_fns <- list.files(path = ".", pattern = "*.R$", full.names = T, recursive = T)
 
+#making an empty df to bind to in the for loop
 df <- data.frame("packages" = as.character(),
                  "functions" = as.character(),
                  "scripts" = as.character())
 
 
+#looping over each script
 for(fn in r_fns){
 
 #  fn <- r_fns[2]
   
   cat(paste0("I'm on ", fn, ".\n"))
   
-x <- NCmisc::list.functions.in.file(filename = fn) %>% 
-  as.data.frame() %>% 
-  rownames_to_column("packages") %>% 
+x <- NCmisc::list.functions.in.file(filename = fn) %>% #listing the functions
+  as.data.frame() %>% #making into df
+  rownames_to_column("packages") %>% #set columns
   rename("functions" = 2) %>% 
-  mutate(packages = str_replace_all(packages, "package:", "")) %>% 
+  mutate(scripts = fn) %>% 
+  mutate(packages = str_replace_all(packages, "package:", "")) %>% #clean up the content in the packages col
   mutate(packages = str_replace_all(packages, "c\\(", "")) %>% 
   mutate(packages = str_replace_all(packages, "\\)", "")) %>% 
   mutate(packages = str_replace_all(packages, "\\\"", "")) %>% 
   mutate(packages = str_replace_all(packages, "character\\(0", "")) %>% 
   mutate(packages = str_split(packages, ",")) %>% 
-  unnest(cols = "packages") %>% 
-  unnest(cols = "functions") %>% 
-  mutate(scripts = fn)
+  unnest(cols = "packages") %>% #split into many rows when there are more than one function or package per row
+  unnest(cols = "functions")  
 
+#joing to outside for loop df
 df <- full_join(x, df, by = c("packages", "functions", "scripts"))
 }
 
+#add col for being used
 used_packages <- df %>% 
   mutate(used = "TRUE")
 
+#make df with all loaded packages (loaded in this script!!)
 loaded_packages <- data.frame(packages = (.packages())) %>% 
   mutate(loaded = "TRUE")
 
+warning("This script will compare the list of used packages to those loaded in the environment and session of this script. If need be, run scripts that load all packages you think you want first to make it a fair comparison.")
+
 joined_df <- full_join(used_packages, loaded_packages)
 
+#report unused but loaded pkgs
 unused_but_loaded <- joined_df %>% 
   filter(is.na(used)) %>% 
   filter(!is.na(loaded)) %>% 
@@ -52,12 +61,12 @@ unused_but_loaded
 
 warning("Note there may be errors, for example the script doesn't tend to understand tidyverse properly but checks each package inside." )
 
+#check which are used the most (as in most unique functions called per package)
 most_used <- used_packages %>% 
   distinct(packages, functions) %>% 
   group_by(packages) %>% 
   summarise(n = n()) %>% 
   arrange(desc(n)) 
-
 
 cat("The top 5 packages from which you use the most different functions are: ")
 most_used[1:5,]
